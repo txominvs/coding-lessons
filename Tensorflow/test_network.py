d6318fb422fe3110266ba1490ad71beeb018618b
Txomin V S <txominvs@gmail.com>
2023-01-20 15:55:17 +0100

    Add comments to first neural network

--- Tensorflow/test_network.py
+++ Tensorflow/test_network.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
+### Generate data
 x_train = []
 y_train = []
-
 import numpy as np
 from numpy.random import default_rng
 rng = default_rng()
@@ -11,11 +11,11 @@ for i in range(10000):
     x_train.append(vals.tolist())
     y_train.append(np.argmax(vals).tolist())
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
 
+### Creating a dataset speeds up the network
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train,)).batch(10)
 
+### Neural Network
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(15, input_shape=(4,), activation='relu'),
     tf.keras.layers.Dense(4, activation='softmax'),
@@ -26,8 +26,8 @@ model.compile(
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     metrics=['acc'])
 
-print(train_dataset)
-
 model.fit(train_dataset, epochs=1)
 
+### Two ways to test the network
 print(model(tf.constant([ [0.25,0.25,0.25,0.25],[0.03,0.9,0.03,0.03], ])))
+print(model.predict([ [0.25,0.25,0.25,0.25],[0.03,0.9,0.03,0.03], ]))
